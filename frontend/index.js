e14a571418342360344a3735f4bf6604006c1e59
Hayley Witherell <hayleywitherell@gmail.com>
2023-12-06 15:51:26 -0700

    Add num records

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,17 @@
 import {
     initializeBlock,
     useBase,
+    useRecords,
 } from '@airtable/blocks/ui';
 import React from 'react';
 
 function TodoExtension() {
     const base = useBase();
+    const table = base.getTableByName('Tasks');
+    const records = useRecords(table);
 
     return (
-        <div>{base.name}</div>
+        <div>Number of tasks: {records.length}</div>
     );
 }
 

5bce0df0906c1fb8fb44b70c6ff631182434716e
Hayley Witherell <hayleywitherell@gmail.com>
2023-12-06 15:50:10 -0700

    Setup js base

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,15 @@
-import {initializeBlock} from '@airtable/blocks/ui';
+import {
+    initializeBlock,
+    useBase,
+} from '@airtable/blocks/ui';
 import React from 'react';
 
-function HelloWorldApp() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+function TodoExtension() {
+    const base = useBase();
+
+    return (
+        <div>{base.name}</div>
+    );
 }
 
-initializeBlock(() => <HelloWorldApp />);
+initializeBlock(() => <TodoExtension />);
